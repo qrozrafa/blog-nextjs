686944178afd253cac2025e14b0b5312dc46e5e8
qrozrafa <rafael_queirozs@hotmail.com>
2023-09-25 21:26:22 -0400

    feat: modulo-07

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Image from 'next/image';
+
+import { allPosts } from 'contentlayer/generated';
+
+import { Mdx } from '@/components/Mdx';
+
 export default function Home() {
+  const posts = allPosts.map((post) => post);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>hello word</h2>
+      <h2>Hellor worldd</h2>
+      <div></div>
     </main>
   );
 }
